72a6a57a587721ff1924d6aea3c84b1400dfd30d
Salavdor <salvador.stereo@gmail.com>
2024-03-27 18:25:29 -0600

    Update info.js

--- src/docs/info.js
+++ src/docs/info.js
@@ -8,7 +8,7 @@ export const info = {
     },
     servers: [
       {
-        url: "http://localhost:8080/api",
+        url: "https://backend-farmacia-p3ix.onrender.com/api",
       },
     ],
   },
